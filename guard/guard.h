ac4a899188f13815cd2f01a432e7d4b6f628ef38
Horst Schirmeier <horst@schirmeier.com>
2025-05-05 15:28:10 +0200

    template for task 3

---
+++ guard/guard.h
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                                G U A R D                                  */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Using this class, we can synchronize activities with a critical section   */
+/* with interrupt handlers that also access this critical section. This      */
+/* synchronization takes place along the prologue/epilogue model.            */
+/*****************************************************************************/
+
+#ifndef __Guard_include__
+#define __Guard_include__
+
+/* Add your code here */ 
+
+class Guard : public Locker {
+public:
+	Guard (const Guard &copy) = delete; // prevent copying
+	Guard& operator=(const Guard&) = delete; // prevent assignment
+	Guard () {}
+/* Add your code here */ 
+};
+
+#endif
---
+++ object/chain.h
@@ -0,0 +1,25 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              C H A I N                                    */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* By inheriting from the Chain class, the derived class inherits a chain    */
+/* pointer that allows its instances to be enqueued in a linked list         */
+/* (implemented in the Queue class).                                         */
+/*****************************************************************************/
+
+#ifndef __chain_include__
+#define __chain_include__
+
+class Chain {
+
+public:
+	Chain(const Chain &copy) = delete; // prevent copying
+	Chain& operator=(const Chain&) = delete; // prevent assignment
+	Chain() {}
+	Chain *next;
+};
+
+#endif
