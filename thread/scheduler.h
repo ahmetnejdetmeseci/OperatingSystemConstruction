be4cdd392280aef5184ded6ff3d9421e8d51b2e4
Horst Schirmeier <horst@schirmeier.com>
2025-05-20 08:53:57 +0200

    template for task 4

---
+++ thread/scheduler.h
@@ -0,0 +1,27 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                          S C H E D U L E R                                */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Scheduler implementation.                                                 */
+/*****************************************************************************/
+
+#ifndef __schedule_include__
+#define __schedule_include__
+
+#include "thread/dispatch.h"
+/* Add your code here */ 
+
+class Scheduler
+/* Add your code here */ 
+{
+public:
+	Scheduler (const Scheduler &copy) = delete; // prevent copying
+	Scheduler& operator=(const Scheduler&) = delete; // prevent assignment
+/* Add your code here */ 
+ 
+};
+
+#endif
