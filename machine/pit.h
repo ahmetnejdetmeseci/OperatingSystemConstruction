93ef9941d616eb0f2622356d52ee77861a4f5310
Horst Schirmeier <horst@schirmeier.com>
2025-06-03 12:54:58 +0200

    template for task 5

---
+++ machine/pit.h
@@ -0,0 +1,29 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                                  P I T                                    */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Programmable Interval Timer.                                              */
+/*****************************************************************************/
+
+#ifndef __pit_include__
+#define __pit_include__
+
+class PIT {
+public:
+	PIT(const PIT &copy) = delete; // prevent copying
+	PIT& operator=(const PIT&) = delete; // prevent assignment
+/* Add your code here */ 
+public:
+	PIT(int us) {
+		interval (us);
+	}
+	int interval() {
+/* Add your code here */ 
+	}
+	void interval(int us);
+};
+
+#endif
---
+++ syscall/thread.h
@@ -0,0 +1,27 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                             T H R E A D                                   */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Implements user interface of a thread.                                    */
+/*****************************************************************************/
+
+#ifndef __thread_include__
+#define __thread_include__
+
+/* Add your code here */ 
+ 
+class Thread
+/* Add your code here */ 
+ 
+{
+public:
+	Thread(const Thread &copy) = delete; // prevent copying
+	Thread& operator=(const Thread&) = delete; // prevent assignment
+/* Add your code here */ 
+ 
+};
+
+#endif
