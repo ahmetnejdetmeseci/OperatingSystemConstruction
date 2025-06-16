e9d5d138fc03c66b729df326db8026c4178af63e
Horst Schirmeier <horst@schirmeier.com>
2025-06-16 11:53:55 +0200

    template for task 6

---
+++ meeting/bell.h
@@ -0,0 +1,28 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                                 B E L L                                   */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Bell is an abstract base class that allows the time-controlled triggering */
+/* of an activity. For this purpose, it has an internal counter that is      */
+/* managed by the Bellringer.                                                */
+/*****************************************************************************/
+
+#ifndef __Bell_include__
+#define __Bell_include__
+
+/* Add your code here */ 
+
+class Bell
+/* Add your code here */ 
+{
+public:
+	Bell(const Bell &copy) = delete; // prevent copying
+	Bell& operator=(const Bell&) = delete; // prevent assignment
+	Bell() {}
+/* Add your code here */ 
+};
+
+#endif
---
+++ meeting/semaphore.h
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                           S E M A P H O R E                               */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* The class Semaphore implements the synchronization concept of the         */
+/* counting semaphore.                                                       */
+/*****************************************************************************/
+
+#ifndef __Semaphore_include__
+#define __Semaphore_include__
+
+/* Add your code here */ 
+
+class Semaphore
+/* Add your code here */ 
+{
+public:
+	Semaphore(const Semaphore &copy) = delete; // prevent copying
+	Semaphore& operator=(const Semaphore&) = delete; // prevent assignment
+/* Add your code here */ 
+};
+
+#endif
